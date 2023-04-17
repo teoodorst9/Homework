66f214e94b89c61d8b12599da131a185a5603e7d
Teodor <126651143+teoodorst9@users.noreply.github.com>
2023-04-17 18:45:07 +0300

    Add files via upload

---
+++ Animal Type/Animal Type/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        switch (animalType)
+        {
+            case "dog":
+                Console.WriteLine("mammal"); break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown"); break;
+        }
+    }
+}
